dbffcffa0a11e4af6fd843132d191a0c673076d4
Vlad <vniemchenko@gmail.com>
2020-10-09 00:32:50 -0400

    v 1.0

--- static/main.js
+++ static/main.js
@@ -17,34 +17,43 @@ let config = {
     messages: []
 }
 
+let options = {
+    options: {
+        debug: false
+    },
+    connection: {
+        secure: true,
+        reconnect: true
+    },
+};
+
+let client = new tmi.client(options);
 
 function connectToTwitch() {
     config.connected = false;
-    let options = {
-        options: {
-            debug: false
-        },
-        connection: {
-            secure: true,
-            reconnect: true
-        },
-    };
-
-    let client = new tmi.client(options);
 
     // Connect the client to the server..
     client.connect().then(
         (msg) => {
             console.log(msg)
 
-            client.join(config.channel).then((msg) => { console.log(`Connected to Twitch [${config.channel}]`); config.connected = true; updateConnectStatus() })
-                .catch(msg => console.error(msg))
-        }).catch(msg => { console.error(msg); config.connected = false; updateConnectStatus() });
+            client.join(config.channel).then((msg) => { console.log(`Connected to Twitch [${config.channel}]`); updateConnectStatus(true) })
+                .catch(msg => { console.error(msg); updateConnectStatus(false) })
+        }).catch(msg => { console.error(msg); updateConnectStatus(false) });
 
     // client.on("connected", () => { console.log(`Connected to Twitch [${config.channel}]`);})
-    client.on("disconnected", () => { console.log(`Disconnected from Twitch`); config.connected = false; updateConnectStatus() });
+    client.on("disconnected", () => { console.log(`Disconnected from Twitch`); updateConnectStatus(false) });
 
     client.on("message", (channel, userstate, message, self) => processMessage(channel, userstate, message, self))
+}
+
+function disconnectFromTwitch() {
+    if (config.connected)
+        client.disconnect().then(
+            () => {
+                updateConnectStatus(false);
+                console.log('Disconnected');
+            })
 }
 
 
@@ -106,9 +115,12 @@ function addToList(user, msg) {
 
     newNode.innerHTML =
         `<th  class='col-1'>${config.messages.length + 1}</th>
-    <td class='col-4'>${newMsg.username}</td>
+    <td class='col-3'>${newMsg.username}</td>
     <td class='col-4'>${newMsg.message}</td>
-    <td class='col-3'>${images}</td>`;
+    <td class='col-3'>${images}</td>
+    <td class="col-1">
+    <button class="btn btn-outline-danger" type="button" onclick="removeMessage('${newMsg.id}')"><i class="far fa-trash-alt"></i></button>
+    </td>`;
     document.getElementById("messagesList").appendChild(newNode);
 
     // fs.appendFileSync('../list.txt', combinedMessage);
@@ -120,7 +132,7 @@ function addToList(user, msg) {
     msgArray.push(newMsg)
     config.messages.push(newMsg)
     console.log("Setting Item")
-    localStorage.setItem('messages', JSON.stringify(JSON.parse(msgArray)));
+    localStorage.setItem('messages', JSON.stringify(msgArray));
 
 
 }
@@ -147,17 +159,55 @@ $(document).ready(function () {
     handleCheckBox('vipCheckBox')
     handleCheckBox('allCheckBox')
 
+
+    if ($("#channel").val() == '') {
+        $("#channel").toggleClass('is-invalid')
+
+        if (!$('.connect-invalid-feedback').hasClass('d-block'))
+            $('.connect-invalid-feedback').addClass('d-block')
+    } else {
+        connectToTwitch();
+        $('.connect-invalid-feedback').removeClass('d-block')
+    }
+    if ($("#command").val() == '') {
+        $("#command").toggleClass('is-invalid')
+    }
+    if ($("#prefix").val() == '') {
+        $("#prefix").toggleClass('is-invalid')
+    }
+
+
     $("#command").on('input propertychange', function () {
+        if ($(this).val() == '')
+            $(this).toggleClass('is-invalid')
+        else
+            $(this).removeClass('is-invalid')
         localStorage.setItem('command', this.value);
         updateConfig();
     })
 
     $("#prefix").on('input propertychange', function () {
+        if ($(this).val() == '')
+            $(this).toggleClass('is-invalid')
+        else
+            $(this).removeClass('is-invalid')
         localStorage.setItem('prefix', this.value);
         updateConfig();
     })
 
     $("#channel").on('input propertychange', function () {
+        disconnectFromTwitch();
+        if ($(this).val() == '') {
+            $(this).toggleClass('is-invalid')
+
+            if (!$('.connect-invalid-feedback').hasClass('d-block'))
+                $('.connect-invalid-feedback').addClass('d-block')
+        }
+        else {
+            $(this).removeClass('is-invalid')
+            $('.connect-invalid-feedback').removeClass('d-block')
+        }
+
         localStorage.setItem('channel', this.value);
         updateConfig();
     })
@@ -198,22 +248,33 @@ function updateCheckBox(checkBoxID) {
 
 }
 
-function updateConnectStatus() {
+function updateConnectStatus(connected) {
+    config.connected = connected;
     console.log("Update Connect Status")
     console.log(config.connected)
     if (config.connected)
-        $('#connect-status').html('✔')
-    else
-        $('#connect-status').html('X')
+        $('#connect-status').html('<i class="fas fa-check text-success"></i>')
+    else {
+        $('#connect-status').html('<i class="fas fa-times text-danger"></i>')
+        $('.connect-invalid-feedback').toggleClass('d-none')
+    }
 }
 
 
-function removeMessage(msg) {
-    let message = config.messages.find(m => m.id == msg.id);
+function removeMessage(msgID) {
+    console.log(`Remove message call for [${msgID}]`)
+    let message = config.messages.find(m => m.id == msgID);
     if (message) {
+        console.log("find message")
         let i = config.messages.indexOf(message);
-        config.messages.slice(i, 1);
+        console.log(config.messages)
+        console.log(i)
+
+        config.messages.splice(i, 1);
+        console.log(config.messages)
         localStorage.setItem('messages', JSON.stringify(config.messages));
+
+        loadListToTable();
     }
 }
 
@@ -221,19 +282,21 @@ function getRandomID() {
     // Math.random should be unique because of its seeding algorithm.
     // Convert it to base 36 (numbers + letters), and grab the first 9 characters
     // after the decimal.
-    return '_' + Math.random().toString(36).substr(2, 9);
+    return '_' + Math.random().toString(36).substr(2, 15);
 };
 
 
 function loadListToTable() {
 
+    $("#messagesList").empty();
+    config.messages = [];
     if (!localStorage.getItem('messages')) return;
 
     let commands = JSON.parse(localStorage.getItem('messages'))
 
     for (let i = 0; i < commands.length; i++) {
         let msg = commands[i];
-        if(!msg) return;4
+        if (!msg) return; 4
         config.messages.push(msg);
 
         let images = ''
@@ -252,11 +315,84 @@ function loadListToTable() {
 
         newNode.innerHTML =
             `<td  class='col-1'>${i}</th>
-        <td class='col-4'>${msg.username}</td>
+        <td class='col-3'>${msg.username}</td>
         <td class='col-4'>${msg.message}</td>
-        <td class='col-3'>${images}</td>`;
+        <td class='col-3'>${images}</td>
+        <td class="col-1">
+    <button class="btn btn-outline-danger" type="button" onclick="removeMessage('${msg.id}')"><i class="far fa-trash-alt"></i></button>
+    </td>`;
 
         document.getElementById("messagesList").appendChild(newNode);
 
     }
 }
+
+
+function saveTxtFile() {
+    let messages = JSON.parse(localStorage.getItem('messages'))
+
+    const today = new Date();
+    
+    const month = ["January", "February", "March", "April", "May", "June", "July",
+        "August", "September", "October", "November", "December"];
+
+    const filename = `subDay_${today.getDate()}_${month[today.getMonth()]}_${today.getFullYear()}.txt`;
+
+    let txtData = ''
+    if (messages) {
+        for (let i = 0; i < messages.length; i++) {
+            let m = messages[i];
+            console.log(m)
+            txtData += `${i}. ${m.username}[${m.badges.join('|')}] - ${m.message}\n`
+        }
+    }
+
+    var blob = new Blob([txtData], { type: "text/plain;charset=utf-8" });
+    saveAs(blob, filename);
+}
+
+function saveExcelFile() {
+    const today = new Date();
+
+    const month = ["January", "February", "March", "April", "May", "June", "July",
+        "August", "September", "October", "November", "December"];
+
+    const badges = ['sub', 'vip', 'broadcaster', 'mod'];
+
+    const filename = `subDay_${today.getDate()}_${month[today.getMonth()]}_${today.getFullYear()}.xlsx`;
+    let data = [['#', 'Username', 'Game', 'Sub', 'Vip', 'Broadcaster', 'Mod']]
+    let messages = JSON.parse(localStorage.getItem('messages'))
+
+    if (messages) {
+        for (let i = 0; i < messages.length; i++) {
+            let m = messages[i];
+            let row = []
+
+            row.push(i)
+            row.push(m.username)
+            row.push(m.message)
+
+            for (let k = 0; k < 4; k++) {
+                if(m.badges.includes(badges[k]))
+                row.push(true)
+                else
+                row.push(false)
+            }
+
+            data.push(row);
+        }
+    }
+
+    const ws_name = "SubDay";
+
+    if (typeof console !== 'undefined') console.log(new Date());
+    const wb = XLSX.utils.book_new(), ws = XLSX.utils.aoa_to_sheet(data);
+
+    /* add worksheet to workbook */
+    XLSX.utils.book_append_sheet(wb, ws, ws_name);
+
+    /* write workbook */
+    XLSX.writeFile(wb, filename);
+}
+
+
